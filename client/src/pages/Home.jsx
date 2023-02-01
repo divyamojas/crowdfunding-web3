eb639ce4c291674e117b6b1c7142c1f24aefadf4
Divyam <divyamojas17@gmail.com>
2023-02-01 19:53:14 +0530

    Add Campaigns to home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import DisplayCampaigns from "../components/DisplayCampaigns";
+import { useStateContext } from "../context";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [isLoading, setIsLoading] = useState(true);
+  const [campaigns, setCampaigns] = useState([]);
 
-export default Home
+  const { address, contract, getCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns(); // we are fetching data here because we cannot await in useEffect
+    setCampaigns(data);
+    setIsLoading(false);
+  };
+  useEffect(() => {
+    if (contract) fetchCampaigns();
+  }, [address, contract]);
+
+  return (
+    <DisplayCampaigns
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  );
+};
+
+export default Home;
